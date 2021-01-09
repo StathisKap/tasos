8e9be7c93203630e91c62bd3135c103a010decb4
StathisKap <stathiskap75@gmail.com>
2021-01-09 06:52:29 +0000

    The Read_Data function now works

--- Ergasia2.c
+++ Ergasia2.c
@@ -8,20 +8,21 @@ struct polites
     float vat1,vat2,vat3;
 };
 
-struct polites* Enter_Data()
+struct polites* Enter_Data(int* num_of_entries)
 {
     int num_of_salesmen;
-    struct polites salesmen_data[num_of_salesmen];
     int i=0;
     char *name_buffer = malloc(30);
  
     printf("Enter the Number of Entries you'd like to add: ");
     scanf("%d",&num_of_salesmen);
+   *num_of_entries = num_of_salesmen; 
+    struct polites *salesmen_data = malloc(num_of_salesmen*sizeof(struct polites));
 
    
-   while(i++ < num_of_salesmen)
+   for(i=0;i < num_of_salesmen;i++)
    {
-       printf("\nEnter the Last Name of Salesman %d\n",i);
+       printf("\nEnter the Last Name of Salesman %d\n",i+1);
        scanf("\n%s",name_buffer);
        strcpy(salesmen_data[i].eponymo,name_buffer);
        
@@ -36,22 +37,23 @@ struct polites* Enter_Data()
   }
 
         printf("\n\nThank You\n\n");
+
+        return salesmen_data;
 };
 
-void Read_Data(struct polites *Data)
+void Read_Data(struct polites *Data,int num_of_entries)
 {
 
-    size_t num_of_salesmen = sizeof(*Data)/sizeof(Data[0]);
     int i,filter;
     
-    printf("Number of Salesmen is %ld. If you'd like to see the ones with an average of over a certain amount,\n"
-    "then Enter that amount below. To exit press 'x'\n",num_of_salesmen);
+    printf("Number of Salesmen is %d. If you'd like to see the ones with an average of over a certain amount,\n"
+    "then Enter that amount below. To exit press 'x'\n",num_of_entries);
 
     if(getchar() == 'x')
     exit(0);
 
     scanf("\n%d", &filter);
-    for(i=0; i<num_of_salesmen; i++)
+    for(i=0; i<num_of_entries; i++)
     {
         int avg = (Data[i].vat1 + Data[i].vat2 + Data[i].vat3)/3;
         if(avg >filter)
@@ -65,8 +67,8 @@ void Read_Data(struct polites *Data)
 int main()
 {
     int num_of_entries;
-    struct polites *Data = Enter_Data();
-    Read_Data(Data);
+    struct polites *Data = Enter_Data(&num_of_entries);
+    Read_Data(Data,num_of_entries);
 
     return 0;
 }
