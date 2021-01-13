90bde9e76f40d17208fe1ef9d5f32c03ecf57e26
StathisKap <stathiskap75@gmail.com>
2021-01-13 04:39:25 +0000

    Finished. Removed the feature where you press 'x' to not enter any values at the Read_Data function. Couldn't get it to work

--- Ergasia4.c
+++ Ergasia4.c
@@ -22,7 +22,6 @@ int main()
 }
 
 
-
 struct players* Enter_Data(int* num_of_entries)
 {
     int num_of_players;
@@ -36,15 +35,25 @@ struct players* Enter_Data(int* num_of_entries)
 
    
    for(i=0;i < num_of_players;i++)
-   {
-       printf("Enter the Code of Player %d\n",i+1);
-       scanf("\n%d",&players_data[i].code);
-
-       printf("Enter the num of Fouls of Player %d\n",i+1);
-       scanf("\n%d",&players_data[i].foul);
+   {    players_data[i].code = -1;
+        players_data[i].foul = -1;
+        players_data[i].points = -1;
+        while (players_data[i].code<0)
+        {
+            printf("\nEnter the Code of Player %d\n",i+1);
+            scanf("\n%d",&players_data[i].code);
+        }
+        while (players_data[i].foul<0)
+        {
+            printf("Enter the number of Fouls of Player %d\n",i+1);
+            scanf("\n%d",&players_data[i].foul);
+        }
         
-       printf("Enter the Points scored by Player %d\n",i+1);
-       scanf("\n%d",&players_data[i].points);
+        while (players_data[i].points<0)
+        {
+            printf("Enter the Points scored by Player %d\n",i+1);
+            scanf("\n%d",&players_data[i].points);
+        }
   }
 
         printf("\n\nThank You\n\n");
@@ -54,23 +63,20 @@ struct players* Enter_Data(int* num_of_entries)
 
 void Read_Data(struct players *Data,int num_of_entries)
 {
-
+    
     int i,filter,filter2=1000000;
     
     printf("Number of Players is %d. If you'd like to see the ones with a number of fouls above a certain number,\n"
-    " and less than a number of points scored, then Enter those numbers below. To exit press 'x'\n",num_of_entries);
+    " or less than a number of points scored, then Enter those numbers below.\n",num_of_entries);
 
-    if(getchar() == 'x')
-    exit(0);
 
-    scanf("\n%d%d", &filter, &filter2);
+    scanf("\n\n%d%d", &filter, &filter2);
     for(i=0; i<num_of_entries; i++)
     {
         if(Data[i].foul>filter || Data[i].points< filter2)
         {
-       printf("\nCode:%d\nFoul:%d\nPoints:%d\n",Data[i].code,Data[i].foul,Data[i].points);
+            printf("\nCode:%d\nFoul:%d\nPoints:%d\n",Data[i].code,Data[i].foul,Data[i].points);
         }
     }
 
 };
-
